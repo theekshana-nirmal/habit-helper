6aa1961e7b07bdf6f5789279574f20d1f1ae9944
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-04-10 13:52:22 +0530

    Add Card component and update styling for Header and Hero sections; enhance Features section with Cards

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -8,12 +8,31 @@ Bullet 3: ⏱️ Quick Start | Most activities take 10 minutes or less to set up
 
 */
 
-import React from 'react'
+import React from "react";
+import Card from "../common/Card";
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center w-full bg-blue-500 text-white py-32 ">
+      <div className="container max-w-7xl flex flex-col items-center text-center">
+        <h2 className="text-3xl text-white font-bold mb-20">Why You’ll Love This</h2>
+        <div className="container max-w-7xl grid grid-cols-3 gap-4">
+          <Card>
+            <h2>🎉 Zero-Cost Fun</h2>
+            <p>All ideas use stuff you already own.</p>
+          </Card>
+          <Card>
+            <h2>🧒 All Ages Welcome</h2>
+            <p>Perfect for kids, teens, and playful adults.</p>
+          </Card>
+          <Card>
+            <h2>⏱️ Quick Start</h2>
+            <p>Most activities take 10 minutes or less to set up.</p>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Features
+export default Features;
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -11,11 +11,11 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div>
-      <div className='flex flex-col items-center justify-center w-full h-screen bg-amber-200 p-4'>
+      <div className='flex flex-col items-center justify-center w-full h-screen bg-white p-4'>
         <div className="container max-w-7xl flex flex-col items-center text-center">
           <h1 className="text-4xl font-bold mb-4">Tired of Endless Scrolling?</h1>
           <p className="text-lg mb-8">Discover simple, refreshing things you can do right now instead of getting lost in your phone. Reclaim your moments, find your focus.</p>
-          <a href="/ideas" className="shadow-md bg-slate-50 text-black px-6 py-3 rounded-lg hover:bg-slate-100 transition duration-300">Explore Activities Now</a>
+          <a href="/ideas" className="shadow-md bg-blue-700 text-white px-6 py-3 rounded-lg hover:bg-blue-800 transition duration-300">Explore Activities Now</a>
         </div>
       </div>
     </div>
