e22526d4a731be5795221785eedb16a1f3347c7a
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-04-10 11:56:04 +0530

    Basic components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import Header from "./components/common/Header";
+import Hero from "./components/sections/Hero";
+import Features from "./components/sections/Features";
+import CTA from "./components/sections/CTA";
+import Footer from "./components/common/Footer";
 
 function App() {
   return (
     <>
+      <Header />
+      <Hero />
+      <Features />
+      <CTA />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
