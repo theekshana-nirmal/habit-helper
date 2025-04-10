f84145741460a3ac0597bec7977a1574b82aa2de
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-04-11 00:06:56 +0530

    Implement routing with React Router for About and Ideas pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/About.jsx";
+import Ideas from "./pages/Ideas.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/ideas",
+    element: <Ideas />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  }
+
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
