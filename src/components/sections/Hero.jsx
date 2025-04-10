7b64b8b2b5cde7fce09dc5884d758ae22b2b4fff
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-04-11 00:06:32 +0530

    Refactor Hero section with updated layout, styling, and navigation link for improved user engagement

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -6,20 +6,43 @@ Sub-headline/Paragraph: Discover simple, refreshing things you can do right now 
 Primary CTA Button Text: Explore Activities Now (Links to Ideas Page)
 */
 
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
-    <div>
-      <div className='flex flex-col items-center justify-center w-full h-screen bg-white p-4'>
-        <div className="container max-w-7xl flex flex-col items-center text-center">
-          <h1 className="mb-8 font-extrabold text-6xl dark:text-blue-600 ">Tired of Endless Scrolling?</h1>
-          <p className="text-lg mb-8">Discover simple, refreshing things you can do right now instead of getting lost in your phone. Reclaim your moments, find your focus.</p>
-          <a href="/ideas" className="shadow-md bg-blue-700 text-white px-6 py-3 rounded-lg hover:bg-blue-800 transition duration-300">Explore Activities Now</a>
+    <section>
+      <div className="max-w-screen-xl mx-auto px-4 py-28 gap-12 text-gray-600 md:px-8">
+        <div className="space-y-10 max-w-4xl mx-auto text-center">
+          <h2 className="text-4xl text-gray-800 font-extrabold mx-auto md:text-5xl">
+          Tired of{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4F46E5] to-[#E114E5]">
+            Endless Scrolling?
+            </span>
+          </h2>
+          <p className="max-w-2xl mx-auto">
+            Discover simple, refreshing things you can do right now instead of
+            getting lost in your phone. Reclaim your moments, find your focus.
+          </p>
+          <div className="items-center justify-center sm:flex">
+            <Link
+              to={"/ideas"}
+              className="block py-2 px-4 text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-full shadow-lg hover:shadow-none"
+            >
+              Explore Activities Now
+            </Link>
+          </div>
+        </div>
+        <div className="mt-14">
+          <img
+            src="https://raw.githubusercontent.com/sidiDev/remote-assets/main/Safari%20(Big%20Sur)%20-%20Light.png"
+            className="w-full shadow-lg rounded-lg border"
+            alt=""
+          />
         </div>
       </div>
-    </div>
-  )
-}
+    </section>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
